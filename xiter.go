4937fd411b5e54f77a3c53c7f55e65079fe797fc
Igor Karatayev <igor.cwer@gmail.com>
2026-06-16 16:59:40 +0300

    added Comparable

--- xiter.go
+++ xiter.go
@@ -33,6 +33,10 @@ type Addable interface {
 	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | uintptr | float32 | float64 | complex64 | complex128 | string
 }
 
+type Comparable interface {
+	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | uintptr | float32 | float64
+}
+
 type Multiplyable interface {
 	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | uintptr | float32 | float64 | complex64 | complex128
 }
